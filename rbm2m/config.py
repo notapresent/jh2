74e36aaef0ff3b3fd3fbcc6024f97f34e3a6a282
notapresent <notapresent@gmail.com>
2015-05-22 17:54:52 +0300

    Add config

---
+++ rbm2m/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+def get_app_env():
+    return os.environ.get('RBM2M_ENV', 'Production')
+
+
+class Config(object):
+    APP_ENV = get_app_env()
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'mysql://rbm2m:rbm2m@localhost/dbm2m'
+    REDIS_URI = 'redis://@localhost:6379/0'
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = os.environ.get('RBM2M_DATABASE_URI')
+    REDIS_URI = os.environ.get('RBM2M_REDIS_URI')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
