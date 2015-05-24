fcfdb2c78e5a370a1f46b982d9e3030dd201da77
notapresent <notapresent@gmail.com>
2015-05-24 22:45:59 +0300

    Clear database before initializing

--- rbm2m/cli.py
+++ rbm2m/cli.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+
 import click
 
-from rbm2m import db, create_app, config
+from rbm2m import create_app
+from rbm2m.models.db import *
 from rbm2m.models.genremanager import GenreManager
+
+app_env = os.environ.get('RBM2M_ENV', 'Production')
 
 
 @click.group()
@@ -12,8 +17,9 @@ def main():
 
 @main.command()
 def initdb():
-    app = create_app(config.app_environment)
+    app = create_app(app_env)
     with app.app_context():
+        db.drop_all()
         db.create_all()
         GenreManager.import_genres(db.session)
     click.echo('Initialized the database')
@@ -21,7 +27,7 @@ def initdb():
 
 @main.command()
 def dropdb():
-    app = create_app(config.app_environment)
+    app = create_app(app_env)
     with app.app_context():
         db.drop_all()
     click.echo('Dropped the database')
