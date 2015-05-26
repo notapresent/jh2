6602e9e1e2f5b7ee5f6d91a1922dfefc27e7306c
notapresent <notapresent@gmail.com>
2015-05-26 09:58:49 +0000

    Implement abort scan

--- rbm2m/models/scheduler.py
+++ rbm2m/models/scheduler.py
@@ -22,8 +22,10 @@ class Scheduler(object):
         self.queue.enqueue('rbm2m.worker.start_scan', genre_id)
 
     def abort_scan(self, scan_id):
-        # TODO
-        pass
+        scan = self.session.query(Scan).filter_by(Scan.id==scan_id).one()
+        scan.status = 'aborted'
+        self.session.add(scan)
+        self.session.commit()
 
     def start_scan(self, genre_id):
         genre = Genre.get(genre_id)
@@ -37,14 +39,22 @@ class Scheduler(object):
         self.queue.enqueue('run_task', scan.to_dict(), genre.title)
 
     def finish_scan(self, scan_dict, pages, status):
-        print "*** Scan #%d finished - %d pages" % (scan_dict['id'], pages+1)
+        print "*** Scan #{} finished with status {} ({} pages)".format(
+            scan_dict['id'], status, pages+1)
         scan = Scan.get(scan_dict['id'])
         scan.status = status
         self.session.add(scan)
         self.session.commit()
 
     def run_task(self, scan_dict, genre_title, page=0):
-        records, nextpage, num_records = scraper.scrape_page(genre_title, page)
+        scan = Scan.get(scan_dict['id'])
+        if scan.status == 'aborted':
+            self.queue.enqueue('finish_scan', scan_dict, page, 'aborted')
+            
+        try:
+            records, next_page, num_records = scraper.scrape_page(genre_title, page)
+        except scraper.ScrapeFailed, e:
+            self.queue.enqueue('finish_scan', scan_dict, page, 'failure')
 
         # records: [(id, {values}), ...]
         ids = [rid for rid, rv in records]
@@ -62,11 +72,10 @@ class Scheduler(object):
         self.session.commit()
         print "Scan #%d page #%d - %d items, %d new" % (scan_dict['id'], page, len(ids), len(ids)-len(existing_ids))
 
-        if nextpage:
+        if next_page:
             self.queue.enqueue('task', scan_dict, genre_title, page+1)
         else:
             self.queue.enqueue('finish_scan', scan_dict, page, 'success')
-
 
 
 class SchedulerError(Exception):
