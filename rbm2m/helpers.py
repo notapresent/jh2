c7a01b27f9b0814048ae5f17d6133614b3c25845
notapresent <notapresent@gmail.com>
2015-05-26 09:24:43 +0000

    Retry decorator

--- rbm2m/helpers.py
+++ rbm2m/helpers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
+import time
+from functools import wraps
 
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
@@ -28,7 +30,7 @@ def make_session(engine=None):
 def make_config(app_env=None):
     if app_env is None:
         os.environ.get('RBM2M_ENV', 'Production')
-    return getattr(config, 'Config'.format(app_env))
+    return getattr(config, 'Config{}'.format(app_env))
 
 
 def get_stats(session):
@@ -44,3 +46,44 @@ def get_stats(session):
     }
 
 
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
