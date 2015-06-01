267d2a402e8d6402705469fe713680068ab51b6a
notapresent <notapresent@gmail.com>
2015-06-01 14:56:42 +0300

    Implement image imported

--- rbm2m/action/image_importer.py
+++ rbm2m/action/image_importer.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from image_manager import ImageManager
+import os
 
+from image_manager import ImageManager
+import scraper
 
 class ImageImporter(object):
     """
-        Imports images
+        Imports images for records
     """
     def __init__(self, config, session):
-        super(ImageImporter, self).__init__()
         self.config = config
         self.session = session
         self._image_manager = None
@@ -21,9 +22,49 @@ class ImageImporter(object):
 
     def run(self, rec_ids):
         """
-
+            Import images for records, identified by rec_ids
         """
-        # multi-download image lists for each record
-        # multi-download images for each record
-        # TODO
-        pass
+        rec_image_urls = scraper.get_image_urls(rec_ids)
+
+        dl_list = self.make_dl_list(rec_image_urls)
+
+        saved = scraper.download_and_save_images(dl_list)
+
+        for img_id, length in saved:
+            img = self.image_manager.get(img_id)
+            img.length = length
+
+    def make_dl_list(self, rec_image_urls):
+        """
+            Generate list of tuples (image_id, image_url, filename)
+        """
+        for rec_id, urls in rec_image_urls.items():
+            img_ids = list(self.save_image_rows(rec_id, urls))
+            yield zip(img_ids, urls, make_img_filenames(img_ids, self.config.MEDIA_DIR))
+
+    def save_image_rows(self, rec_id, urls):
+        """
+            Insert images for record and yield inserted row ids
+        """
+        for url in urls:
+            img = self.image_manager.from_dict({'record_id': rec_id, 'url': url})
+            yield img.id
+
+
+def make_img_filenames(img_ids, basedir):
+    """
+        Generate a sequence of make_img_filenames from ids
+    """
+    for img_id in img_ids:
+        yield make_filename(img_id, basedir)
+
+
+def make_filename(img_id, basedir='', suffix='.jpg'):
+    """
+        Generate filename from id and base dir, with optional suffix
+    """
+    strid = str(img_id).zfill(6)
+    frags = [strid[i:i+2] for i in range(0, len(strid), 2)]
+    frags.append("{}{}".format(img_id, suffix))
+    frags.insert(0, basedir)
+    return os.path.join(*frags)
