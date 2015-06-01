4549ceda7536e6620bed84eaf05f5d246a083a3a
notapresent <notapresent@gmail.com>
2015-06-01 21:04:18 +0300

    Fix constructor argument name

--- rbm2m/action/task_queue.py
+++ rbm2m/action/task_queue.py
@@ -10,15 +10,14 @@ class TaskQueue(object):
         Abstracts task queue
     """
     def __init__(self, redis=None):
-        self.q = rq.Queue(redis_conn=redis)
+        self.q = rq.Queue(connection=redis, default_timeout=JOB_TIMEOUT)
 
     def enqueue(self, task_name, *args, **kwargs):
         """
             Add task with specified name and arguments to task queue
         """
-        timeout = kwargs.pop('timeout', JOB_TIMEOUT)
+        # timeout = kwargs.pop('timeout', JOB_TIMEOUT)
         at_front = kwargs.pop('at_front', False)
-        job = self.q.enqueue('worker.run_task', task_name,
-                             timeout=timeout, at_front=at_front,
+        job = self.q.enqueue('worker.run_task', task_name, at_front=at_front,
                              *args, **kwargs)
         return job
