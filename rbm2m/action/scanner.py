20df167f3278eb85e40ecfb962dfa338328eb411
notapresent <notapresent@gmail.com>
2015-06-02 00:24:43 +0300

    Print some informational messages

--- rbm2m/action/scanner.py
+++ rbm2m/action/scanner.py
@@ -85,6 +85,7 @@ class Scanner(object):
         """
             Import records from page and queue next page and/or image import
         """
+        print "*** Page task started. Scan #{}/page #{}".format(scan_id, page_no or 0)
         scan = self.scan_manager.get(scan_id)
         if scan.status == 'aborted':     # or != 'running'
             return
@@ -95,6 +96,7 @@ class Scanner(object):
             imp.run(scan, page_no)
         except record_importer.RecordImportError as e:
             self.finish_scan(scan.id, 'failed')
+            print "*** page task failed: {}".format(e)
             return
 
         if imp.has_images:
@@ -104,6 +106,8 @@ class Scanner(object):
             self.queue.enqueue('page_task', scan.id, imp.next_page)
         else:
             self.finish_scan(scan.id, 'success')
+
+        print "*** page task completed. Scan #{}/page #{}".format(scan_id, page_no)
 
     def image_task(self, rec_ids):
         """
