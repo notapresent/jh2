2a402cc0a0f87c99d3f2534f3cf2ba83e68afe14
notapresent <notapresent@gmail.com>
2015-06-04 23:07:34 +0300

    Auto-activate virtualenv on production

--- runworker.py
+++ runworker.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-import redis
+import os
 
+app_env = os.environ.get('RBM2M_ENV', 'Production')
+
+if app_env == 'Production':
+    venv_path = os.path.join(os.path.dirname(__file__), 'venv')
+    activate_this = os.path.join(venv_path, 'bin/activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+
+
+import redis
 from rq import Worker, Queue, Connection
 
 from rbm2m.helpers import make_config
 
-# TODO pre-import modules here
-
 config = make_config()
-
 listen = [config.RQ_QUEUE_NAME]
 conn = redis.from_url(config.REDIS_URL)
 
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
-from rbm2m.webapp import create_app
 
 app_env = os.environ.get('RBM2M_ENV', 'Production')
+
+if app_env == 'Production':
+    venv_path = os.path.join(os.path.dirname(__file__), 'venv')
+    activate_this = os.path.join(venv_path, 'bin/activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+
+from rbm2m.webapp import create_app
+
 app = create_app(app_env)
 
 if __name__ == "__main__":
