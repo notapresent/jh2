0570f387e76110864181ce3fea4961648cef805d
notapresent <notapresent@gmail.com>
2015-06-21 21:58:15 +0300

    Do not activate virtualenv if is not exists

--- runworker.py
+++ runworker.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 
+
+base_dir = os.path.dirname(__file__)
+venv_path = os.path.join(base_dir, 'venv')
+
 app_env = os.environ.get('RBM2M_ENV', 'Production')
 
-if app_env == 'Production':
+if app_env == 'Production' and os.path.isdir(venv_path):
     venv_path = os.path.join(os.path.dirname(__file__), 'venv')
     activate_this = os.path.join(venv_path, 'bin/activate_this.py')
     execfile(activate_this, dict(__file__=activate_this))
