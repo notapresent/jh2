d976da8addb62650320ee9d1ef31c332829aed6e
notapresent <notapresent@gmail.com>
2015-07-02 23:53:40 +0000

    Implement streaming yml and table exports

--- rbm2m/views/public.py
+++ rbm2m/views/public.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from flask import Blueprint, render_template, request, send_from_directory, current_app
+from flask import (Blueprint, render_template, request, send_from_directory,
+                   current_app, Response, stream_with_context)
 
 from ..webapp import db
 from ..action import exporter
@@ -18,14 +19,13 @@ def yml():
     """
     exp = exporter.YMLExporter(db.session)
     exp.log_export(client_ip(), request.user_agent)
-
     ctx = {
         'generation_date': exp.generation_date(),
         'genres': exp.category_list(),
         'offers': exp.offers()
     }
+    return Response(stream_with_context(stream_template('yml.xml', **ctx)))
 
-    return render_template('yml.xml', **ctx)
 
 @bp.route('/table')
 def table():
@@ -39,8 +39,7 @@ def table():
         'genres': exp.category_list(),
         'rows': exp.rows()
     }
-
-    return render_template('table.html', **ctx)
+    return Response(stream_with_context(stream_template('table.html', **ctx)))
 
 
 @bp.route('/media/<path:path>')
@@ -56,3 +55,14 @@ def client_ip():
         return request.environ['HTTP_X_REAL_IP']
 
     return request.remote_addr
+
+
+def stream_template(template_name, **context):
+    """
+        Stream rendered template output
+    """
+    current_app.update_template_context(context)
+    t = current_app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
